86f3c4b5e2c6cdcb83625753fca80799ee4faea4
fabienfrfr <69802483+fabienfrfr@users.noreply.github.com>
2020-11-06 17:15:09 +0100

    Update CSV_PLOT.py

--- CSV_PLOT.py
+++ CSV_PLOT.py
@@ -10,44 +10,43 @@ from sklearn import preprocessing
 
 #################################### PARAM :
 ## CSV path
-PATH = r'/media/fabien/F6166FAF166F7013/0_UNIX/DEV/Python_scripts/TRACKER_project/171205_Stack-fitted/'
-IS_SPLITED = True
-EXP_PARAM = 'experiment-log' # String 'imageTimes', 'experiment-log' or 'None'
-IS_CCC = True # MATLAB LOG VERSION (need to convert in csv)
+PATH = r'/media/fabien/F6166FAF166F7013/0_THESE/ARTICLE/200105/'
+IS_SPLITED = False
+EXP_PARAM = 'imageTimes' # String 'imageTimes', 'experiment-log' or 'None'
+IS_CCC = False # MATLAB LOG VERSION (need to convert in csv)
 
 ## Time and Feed factor (if no exp_param file)
 T_MIN, T_MAX = 24, 96
-IS_FEED = False
+IS_FEED = True
 
 ## Columns of CSV  (see 1st line of csv)
-CONDITION = np.array([None])
-CELL_INFO = np.array(['frame', 'x', 'y', 'I', 'w', 'L'])
+CONDITION = np.array(['C0', 'C1', 'C2', 'C3', 'C4'])
+CELL_INFO = np.array(['frame', 'cell', 'x', 'y', 'I_nucleus', 'I_cytoplasm', 'ratio'])
 
 ## Additional columns (if is splitted : no info in log)
 NB_PLICAT = 2
 NEW_COND = {'C0' : ['K', 'AFK', '20S', '40S', '80S', '20P', '40P', '80P', '20-5P', '20-10P', '20-20P', '20-40P']} #180726
-"""
-{'C0' : ['K', 'AFK', '20S', '40S', '80S', '20P', '40P', '80P', '20-5P', '20-10P', '20-20P', '20-40P']} #180726
-{'C0' : [20, 10, 5, 3.3, 1.5, 0.03, 0.1, 0.33, 1., 3., 5.5, 6.5]} # 2018-07-27
-"""
+
 # Figure
 W, H, L, S = 3.7, 2.9, 18., 9. # width, height, label_size, scale_size
-VAL_ = [0., 1.1]
-T = [0, 120]
+VAL_ = [0.75, 2.]
+T = [0, 60]
+EXP_IDX = np.array([9,8,3])# values or None
+STD = np.pi
 
 # Stat-param
-OUT_QUANTILE = 0.995
+OUT_QUANTILE = 0.999
 
-## Plotting classification
-'''Cond savefig sep -> curve per condition -> X_axis grouping -> Y_axis to see'''
+## Plotting classification (hierarchy info)
+'''Cond savefig sep -> curve per condition -> X_axis grouping -> Y_axis to see (2 if ratio)'''
 # Indicate index (see Condition and cell info order)
-IDX_SAVE_CURVE = [0, 1]
-IDX_AXIS = [0,3]
-IDX_LABEL = [1]
+IDX_SAVE_CURVE = [1, 0] #cond
+IDX_AXIS = [0,-1] #cell
+IDX_LABEL = [3] #cond
 
 ## Info variable output
-NAME_VAR = 'ASE-YFP'
-VAR_ = '(norm)'
+NAME_VAR = 'Smad2-RFP¨'
+VAR_ = 'ratio' # (norm) or ratio
 
 ###############################################################################
 #################################### INITIALISATION :
@@ -64,7 +63,7 @@ LABEL = CONDITION[IDX_LABEL]
 # Import csv
 if IS_SPLITED :
     # Path listing
-    List_csv_path = [PATH + f for f in sorted(os.listdir(PATH)) if (".csv" in f[-4:] and not(EXP_PARAM) in f)]
+    List_csv_path = [PATH + f for f in sorted(os.listdir(PATH)) if (".csv" in f[-4:] and not(EXP_PARAM in f))]
     # Adding new condition
     DATA, n = [], 0
     for i in range(len(List_csv_path)) :
@@ -79,7 +78,7 @@ if IS_SPLITED :
         DATA += [data]
     DATA = pd.concat(DATA)
 else :
-    List_csv_path = [PATH + f for f in sorted(os.listdir(PATH)) if (".csv" in f[-4:] and not(EXP_PARAM) in f[-4:])]
+    List_csv_path = [PATH + f for f in sorted(os.listdir(PATH)) if (".csv" in f[-4:] and not(EXP_PARAM in f))]
     DATA = pd.read_csv(List_csv_path[0])
 
 # Extract name experiment
@@ -88,7 +87,7 @@ CURVE_FOLDER = PATH.split(os.path.sep)[-2]
 if(not os.path.isdir(CURVE_FOLDER)): os.makedirs(CURVE_FOLDER)
 # Parameter verification and Affect name str
 idx2test = np.where(LVL_ORDER != None)[0]
-if False in [n in DATA.columns for n in LVL_ORDER[idx2test]] : sys.exit("Error parameter")
+if False in [n in DATA.columns for n in LVL_ORDER[idx2test]] : sys.exit("Error parameter !!!!")
 
 #################################### TIME-FEED SET :
 if 'None' in EXP_PARAM :
@@ -102,10 +101,10 @@ elif IS_CCC :
     T_MIN, T_MAX = EXP_INFO['t'].min(), EXP_INFO['t'].max()
     frame_ = np.unique(DATA.frame)
     t = T_MIN + (T_MAX-T_MIN) * frame_ / (frame_.size - 1)
-else :
+elif IS_FEED :
     # Manual version
     csv_exp_param = [PATH + f for f in os.listdir(PATH) if EXP_PARAM in f]
-    EXP_INFO = pd.read_csv(csv_exp_param, delimiter=';')
+    EXP_INFO = pd.read_csv(csv_exp_param[0], delimiter=';')
     t = EXP_INFO['image times'].values
     feed = EXP_INFO['feed times'].values
     feed = feed[np.invert(np.isnan(feed))]
@@ -115,8 +114,11 @@ else :
 outlier = DATA[LVL_ORDER[-1]].quantile(OUT_QUANTILE) # 99,5% of statistical sample follow 'I'
 DATA = DATA[DATA[LVL_ORDER[-1]] < outlier]
 
-min_max_scaler = preprocessing.MinMaxScaler()
-DATA[LVL_ORDER[-1]] = min_max_scaler.fit_transform(DATA[[LVL_ORDER[-1]]].values)
+if VAR_ == '(norm)' :
+    min_max_scaler = preprocessing.MinMaxScaler()
+    DATA[LVL_ORDER[-1]] = min_max_scaler.fit_transform(DATA[[LVL_ORDER[-1]]].values)
+elif VAR_ == 'ratio' :
+    DATA[LVL_ORDER[-1]] = DATA['I_nucleus']/DATA['I_cytoplasm'] ## NOT GOOD !! 
 plt.hist(DATA[LVL_ORDER[-1]].values, 100); plt.show(); plt.close() # Poisson distrib, peak at ~0.1 (no signal)
 
 #################################### HIERARCHY PLOT :
@@ -143,12 +145,16 @@ for DL in DATA_LOOP :
     
     ## Type of VAR output
     if VAR_ == '(norm)' :
-        # second Normalisation (mean norm)
+        # Second Normalisation (mean norm)
         min_val, max_val = curve.min().min(), curve.max().max() + fill.max().max()/2
         curve = (curve - min_val)/(max_val-min_val)
-    
+    elif VAR_ == 'ratio' :
+        # Fluorescence correction
+        delta_val = 1-curve[0].min()
+        if delta_val < 0 : curve = curve - abs(delta_val)
     ## Curve labelling
-    lbl_list = curve.index.values.astype('str')
+    gf_lbl = df.groupby([LVL_ORDER[1]])
+    lbl_list = gf_lbl[LABEL].first().values.astype('str') # normally same name
     
     # Figure
     fig = plt.figure(figsize=(W, H))
@@ -162,11 +168,13 @@ for DL in DATA_LOOP :
     ax.set_xlabel('time (h)', fontsize=L)
     #curve.T.plot(ax=ax) # Automatic legends, otherwise : #name = curve.index.values
     i = 0
-    for stack in np.dstack((curve.values, fill, time)) :
-        name = lbl_list[i]; i+=1
+    if not(np.any(EXP_IDX != None)) : 
+        EXP_IDX = np.arange(lbl_list.size)
+    for stack in np.dstack((curve.values, fill, time))[EXP_IDX] :
+        name = lbl_list[EXP_IDX[i]]; i+=1
         z = stack.T
         ax.plot(z[-1], z[0], label=name)
-        ax.fill_between(z[-1], z[0] - z[1]/2, z[0] + z[1]/2, alpha=0.3)
+        ax.fill_between(z[-1], z[0] - z[1]/STD, z[0] + z[1]/STD, alpha=0.3)
     
     # feed bar :
     if IS_FEED :
